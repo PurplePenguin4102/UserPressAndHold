257e1a9acf6bcf9c96bc29601ea0c73b744c1f8c
Joey <joseph.h.ray@gmail.com>
2017-06-09 17:00:51 +1000

    initial commit

---
+++ Stopwatch/BackgroundColourConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Stopwatch
+{
+    public class BackgroundColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            long val = (long)value;
+            return (val > 3000) ? "Bisque" : "ForestGreen";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stopwatch/Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StopwatchUI
+{
+    public class Model : INotifyPropertyChanged
+    {
+        public long ShownTime
+        {
+            get => stopwatch.ElapsedMilliseconds;
+        }
+
+        public Model() => LatestStopwatch();
+
+        private async void LatestStopwatch()
+        {
+            while (true)
+            {
+                long oldTime = ShownTime;
+                await Task.Delay(TimeSpan.FromMilliseconds(10));
+                if (oldTime != ShownTime)
+                    OnPropertyChanged(nameof(ShownTime));
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void StartTimer()
+        {
+            stopwatch.Start();
+        }
+
+        public void StopTimer()
+        {
+            stopwatch.Stop();
+        }
+
+        public void ResetTimer()
+        {
+            stopwatch.Reset();
+        }
+    }
+}
