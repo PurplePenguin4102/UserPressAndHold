873ec0b2e4e3a4fa518ebbd026da73fdd51d6d6f
Joseph Ray <joseph.h.ray@gmail.com>
2017-06-10 11:10:59 +1000

    COMMENTS

--- Stopwatch/Model.cs
+++ Stopwatch/Model.cs
@@ -16,17 +16,35 @@ namespace StopwatchUI
 {
     public class Model : INotifyPropertyChanged
     {
+        // the worker object
         private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-
-        public Model() => new Timer(UpdateUI, stopwatch, 10, 10);
-        public long ShownTime { get => stopwatch.ElapsedMilliseconds; }
+        // we need this guy to compare to stopwatch.ElapsedMillis
         private long oldTime = 0;
+
+        /// <summary>
+        /// Starts a timer to update UI thread with stopwatch value
+        /// </summary>
+        public Model() => new Timer(UpdateUI, stopwatch, 10, 10);
+
+        /// <summary>
+        /// The stopwatch's current value in milliseconds
+        /// </summary>
+        public long ShownTime { get => stopwatch.ElapsedMilliseconds; }
+
+        // we keep this guy so that we can notify the UI from the timer thread
         private CoreDispatcher Dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
+
+        // implement INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
+
+        // wrappers for stopwatch functionality
         public void StartTimer() => stopwatch.Restart();
         public void StopTimer() => stopwatch.Stop();
         public void ResetTimer() => stopwatch.Reset();
 
+        /// <summary>
+        /// Our timer callback. Compares the stopwatch with the stored time and updates UI thread when different
+        /// </summary>
         private async void UpdateUI(object state)
         {
             if (oldTime != ShownTime)
@@ -34,9 +52,9 @@ namespace StopwatchUI
                 await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => OnPropertyChanged(nameof(ShownTime)));
                 oldTime = ShownTime;
             }
-            
         }
 
+        // wrap the event handler with this utility. Don't need to populate if called from a property
         private void OnPropertyChanged([CallerMemberName]string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
